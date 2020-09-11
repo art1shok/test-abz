6fab0be86b815e8c79f60c543cec9c243ca45ff2
Artem Kvitka <kvitkaart554@gmail.com>
2020-09-11 18:08:40 +0300

    Updates

---
+++ precache-manifest.4196e5db0d4bdc5ab03f00d491901b81.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0926de7193ce3e7f369f5cca74f8b078",
+    "url": "/test-abz/index.html"
+  },
+  {
+    "revision": "dd1171e99f79c4be64d0",
+    "url": "/test-abz/static/css/main.8a82ff83.chunk.css"
+  },
+  {
+    "revision": "67736f688ff010b85d6b",
+    "url": "/test-abz/static/js/2.dbb718c6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/test-abz/static/js/2.dbb718c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd1171e99f79c4be64d0",
+    "url": "/test-abz/static/js/main.1e56e5ef.chunk.js"
+  },
+  {
+    "revision": "bde0c319750e5021602f",
+    "url": "/test-abz/static/js/runtime-main.91d965c1.js"
+  },
+  {
+    "revision": "38ca9e2302c8793c93f854e8595eed78",
+    "url": "/test-abz/static/media/OpenSans-Regular.38ca9e23.eot"
+  },
+  {
+    "revision": "5d5735e57127db2f7a2ad879fc6056b8",
+    "url": "/test-abz/static/media/OpenSans-Regular.5d5735e5.woff2"
+  },
+  {
+    "revision": "6fde2eb6728eb97fd914dcd0641d332b",
+    "url": "/test-abz/static/media/OpenSans-Regular.6fde2eb6.woff"
+  },
+  {
+    "revision": "8df00d6176ddae387d9431d3307b3e62",
+    "url": "/test-abz/static/media/OpenSans-Regular.8df00d61.ttf"
+  },
+  {
+    "revision": "7106bb83a0e767e12659de9cfba3926d",
+    "url": "/test-abz/static/media/OpenSans-SemiBold.7106bb83.woff2"
+  },
+  {
+    "revision": "e0d5280b186197bd2a77b9babfe5506d",
+    "url": "/test-abz/static/media/OpenSans-SemiBold.e0d5280b.eot"
+  },
+  {
+    "revision": "f28ed2e40f2a6955ddc5dd3f5088a5a2",
+    "url": "/test-abz/static/media/OpenSans-SemiBold.f28ed2e4.ttf"
+  },
+  {
+    "revision": "fe241c6c4f5a190e821b7c03e337d756",
+    "url": "/test-abz/static/media/OpenSans-SemiBold.fe241c6c.woff"
+  },
+  {
+    "revision": "f5443d74025f2a6069e0ca0142c08a05",
+    "url": "/test-abz/static/media/banner-photo.f5443d74.jpg"
+  },
+  {
+    "revision": "bce28fb772838de34344b20fa26296b0",
+    "url": "/test-abz/static/media/logo.bce28fb7.svg"
+  },
+  {
+    "revision": "f28cb247d5778a3e9e6e5be021b9a81b",
+    "url": "/test-abz/static/media/man-laptop-v1.f28cb247.svg"
+  },
+  {
+    "revision": "63637e948daaff9637f3f5a803d2b669",
+    "url": "/test-abz/static/media/menu icon.63637e94.svg"
+  },
+  {
+    "revision": "a33f2d3cee0ddafd303461d319e9a2ab",
+    "url": "/test-abz/static/media/photo-cover.a33f2d3c.svg"
+  }
+]);
