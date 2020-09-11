be0ff9152a4a8e1f1e1884ed2d59623acc7d410e
Artem Kvitka <kvitkaart554@gmail.com>
2020-09-11 20:38:25 +0300

    fixed tooltip

--- src/components/Tooltip/Tooltip.js
+++ src/components/Tooltip/Tooltip.js
@@ -1,10 +1,24 @@
-import React from 'react';
+import React, { useRef, useEffect, useState } from 'react';
 import './Tooltip.scss';
 
-const Tooltip = ({ content }) => (
-  <div className="tooltip">
-    {content}
-  </div>
-);
+const Tooltip = ({ content }) => {
+  const ref = useRef();
+  const [style, setStyle] = useState({});
+
+  useEffect(() => {
+    if (ref.current) {
+      const { right } = ref.current.getBoundingClientRect();
+      if (right > window.innerHeight + 20) {
+        setStyle({ left: 'auto', right: 0 });
+      }
+    }
+  }, []);
+
+  return (
+    <div className="tooltip" ref={ref} style={style}>
+      {content}
+    </div>
+  );
+};
 
 export default Tooltip;
--- src/components/UserCard/UserCard.js
+++ src/components/UserCard/UserCard.js
@@ -1,57 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import './UserCard.scss';
 import Tooltip from '../Tooltip/Tooltip';
 import defaultImage from '../../assets/images/photo-cover.svg';
 
-const cutText = (string, length) => {
-  if (string) {
-    if (string.length <= length) {
-      return string;
-    }
-
-    return (
-      <span className="user-card__tooltip-wrapper">
-        {string.substring(0, length - 1)}
-        ...
-        <Tooltip content={string} />
-      </span>
-    );
-  }
-
-  return '';
-};
-
 const UserCard = (props) => {
   const {
-    name,
-    position,
-    email,
-    imageUrl,
-    phone,
+    name, position, email, imageUrl, phone,
   } = props;
+
+  const [tooltipText, setTooltipText] = useState('');
+
+  const cutText = (string, length) => {
+    if (string) {
+      if (string.length <= length) {
+        return string;
+      }
+
+      return (
+        <span
+          tabIndex={0}
+          className="user-card__tooltip-wrapper"
+          onFocus={() => setTooltipText(string)}
+          onMouseOver={() => setTooltipText(string)}
+          onBlur={() => setTooltipText('')}
+          onMouseOut={() => setTooltipText('')}
+        >
+          {string.substring(0, length - 1)}
+          ...
+          {tooltipText === string && <Tooltip content={string} />}
+        </span>
+      );
+    }
+
+    return '';
+  };
 
   return (
     <div className="user-card">
       <div className="user-card__content">
-
-        <img
-          src={imageUrl}
-          alt={name}
-          className="user-card__img"
-        />
-        <h2 className="user-card__name">
-          {cutText(name, 15)}
-        </h2>
-        <div className="user-card__position">
-          {position}
-        </div>
-        <div className="user-card__email">
-          {cutText(email, 21)}
-        </div>
-        <div className="user-card__phone">
-          {cutText(phone, 21)}
-        </div>
+        <img src={imageUrl} alt={name} className="user-card__img" />
+        <h2 className="user-card__name">{name}</h2>
+        <div className="user-card__position">{position}</div>
+        <div className="user-card__email">{cutText(email, 21)}</div>
+        <div className="user-card__phone">{cutText(phone, 21)}</div>
       </div>
     </div>
   );
